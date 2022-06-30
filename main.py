20c39a33098e8f80e0988a453e17e8a3cf5147a5
Andrew Koh <andrewkohjinjie@hotmail.com>
2022-06-30 17:43:40 +0800

    takes in folder as d input. creates srt file for each audio

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ import argparse
 from utils import set_device
 import yaml
 from easydict import EasyDict
+import srt
+from datetime import timedelta
+import os
+from tqdm import tqdm
 
 with open('config.yaml', "rb") as stream:
     config = EasyDict(yaml.full_load(stream))
 parser = argparse.ArgumentParser(description='args for streaming model')
-parser.add_argument('-d', '--demo', type=str, default='test_3sec.wav')
+parser.add_argument('-d', '--demo', type=str, default='test_3sec.wav') # path or dir to mp4
 parser.add_argument('-n', '--num_frames', type=int, default=config.num_frames)
 parser.add_argument('-t', '--threshold', type=float, default=config.threshold)
 parser.add_argument('-g', '--gpu', type=bool, default=config.gpu)
@@ -34,14 +38,31 @@ model = StreamingModel(config)
 
 def main():
 
-    # load example wav file
-    wav = librosa.load(config.demo , sr=config.sample_rate)[0]
+    if os.path.isfile(config.demo):
+        paths = [config.demo]
+    else:
+        assert os.path.isdir(config.demo)
+        paths = [os.path.join(config.demo,p) for p in os.listdir(config.demo)]
 
-    # for every 3 second wav, pass that segment to predict_3sec
-    for curr_window, curr_frame in enumerate(range(0,len(wav), config.sample_rate*3)):
-        # send 3 sec segment to model for prediction
-        predictions = model.predict_3sec(wav[curr_frame:curr_frame+(config.sample_rate*3)], config.k)
-        print(f"{curr_window}:, {predictions}")
+    for p in tqdm(paths):
+        if p[-4:] not in ['.mp3', '.mp4', '.wav']:
+            continue
+        wav = librosa.load(p , sr=config.sample_rate)[0]
+        srts = []
+
+        # for every 3 second wav, pass that segment to predict_3sec
+        for curr_window, curr_frame in enumerate(range(0,len(wav), config.sample_rate)):
+            # send 3 sec segment to model for prediction
+            predictions = model.predict_3sec(wav[curr_frame:curr_frame+(config.sample_rate*3)], config.k)
+            # print(f"{curr_window}:, {predictions}")
+            formatted_preds = ''
+            for i in range(config.k):
+                formatted_preds += f"{i}: {predictions[i]} "
+            srts.append(srt.Subtitle(index=None,start=timedelta(seconds=curr_window), end=timedelta(seconds=curr_window+1), content=formatted_preds))
+        srts = srt.compose(srts, reindex=True)
+        srt_save_path = '.'.join(p.split(".")[:-1]) + '.srt'
+        with open(srt_save_path,'w') as f:
+            f.write(srts) 
 
     # example output predictions: ['chatter', 'others', 'screaming', 'motor_vehicle_road', 'emergency_vehicle']
 
